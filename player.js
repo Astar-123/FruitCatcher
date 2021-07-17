88c46ad71b60a22a364b9dc4af42e7ab2f4851fe
Astar-123 <76821022+Astar-123@users.noreply.github.com>
2021-07-17 07:46:38 -0400

    Add files via upload

---
+++ player.js
@@ -0,0 +1,53 @@
+class Player {
+    constructor() {
+        this.index = null;
+        this.distance = 0;
+        this.name = null;
+          // create the property for the score and intialize with zero
+        this.score=0;
+    }
+
+    getCount() {
+        var playerCountRef = database.ref('playerCount');
+        playerCountRef.on("value", (data) => {
+            playerCount = data.val();
+        })
+    }
+
+    updateCount(count) {
+        database.ref('/').update({
+            playerCount: count
+        });
+    }
+
+    update() {
+        var playerIndex = "players/player" + this.index;
+        database.ref(playerIndex).set({
+            name: this.name,
+            distance: this.distance,
+            score:this.score
+        });
+    }
+
+    static getPlayerInfo() {
+        var playerInfoRef = database.ref('players');
+        playerInfoRef.on("value", (data) => {
+            allPlayers = data.val();
+        })
+    }
+
+    getScore() {
+        var scoreRef= database.ref('score');
+        scoreRef.on("value",(data) =>{
+            score= data.val();
+        })
+    }
+
+    updateScore() {
+        database.ref('/').update({
+            score:this.score
+        })
+    }
+
+    
+}
